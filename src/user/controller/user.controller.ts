09343a4173fda713d2d842075a14f9969b0d1347
AlejandroMontes2303 <alejandroliveiramontes@outlook.com>
2021-02-07 22:40:27 -0300

    Create User CRUD

--- src/user/controller/user.controller.ts
+++ src/user/controller/user.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Get,
+    Post,
+    Delete,
+    Put,
+    Param,
+  } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { User } from '../models/user.interface';
+import { UserService } from '../service/user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(
+    private userService: UserService,
+  ){}
+
+  @Post()
+  Create(@Body() user:User): Observable<User> {
+    return this.userService.create(user)
+  }
+
+  @Get(':id')
+  Find(@Param() params): Observable<User>{
+    return this.userService.find(params.id)
+  }
+
+  @Get()
+  FindAll(): Observable<User[]>{
+    return this.userService.findAll()
+  }
+
+  @Put(':id')
+  Update(@Param() id: string, @Body() user:User): Observable<any>{
+    return this.userService.update(Number(id), user)
+  }
+
+  @Delete(':id')
+  delete(@Param() id: string): Observable<any>{
+    return this.userService.delete(Number(id))
+  }
+}
